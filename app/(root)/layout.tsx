b6f16cdc87fc5330c4df752335d86372dc3cfe9d
Eml-coder <108040528+Eml-coder@users.noreply.github.com>
2023-08-03 15:21:27 -0400

    start sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,16 @@
 import Navbar from "@/components/Navbar";
+import { Sidebar } from "@/components/Sidebar";
+//try to add a sidebar
 
 const RootLayout = ({
     children }: { children: React.ReactNode }) => {
     return (
         <div className='h-full'>
             <Navbar />
+            <div className='hidden md:flex mt-20 w-20 flex-col fixed inset-y-0'>
+                <Sidebar />
+
+            </div>
             <main className='md:pl-20 pt-16 h-full'>
             {children}
             </main>
