13df4fca1061493b2d8c4c0fc0d42b3e2981513e
Eml-coder <108040528+Eml-coder@users.noreply.github.com>
2023-08-14 14:59:11 -0400

    setup forms for name, description, and image upload

--- app/(root)/(routes)/companion/components/companionForm.tsx
+++ app/(root)/(routes)/companion/components/companionForm.tsx
@@ -4,9 +4,25 @@ import * as z from 'zod';
 import { Category, Companion } from '@prisma/client';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Form, FormField, FormItem, FormControl, FormMessage } from '@/components/ui/form';
+import {
+	Form,
+	FormField,
+	FormItem,
+	FormControl,
+	FormMessage,
+	FormLabel,
+	FormDescription,
+} from '@/components/ui/form';
 import { Separator } from '@/components/ui/separator';
 import { ImageUpload } from '@/components/ImageUpload';
+import { Input } from '@/components/ui/input';
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select';
 
 interface CompanionFormProps {
 	initialData: Companion | null;
@@ -64,11 +80,91 @@ export const CompanionForm = ({
 						name='src'
 						render={({ field }) => (
 							<FormItem className='flex flex-col items-center justify-center space-y-4'>
-								<FormControl><ImageUpload disabled={isLoading} onChange={field.onChange} value={field.value}/></FormControl>
-                                <FormMessage  />
+								<FormControl>
+									<ImageUpload
+										disabled={isLoading}
+										onChange={field.onChange}
+										value={field.value}
+									/>
+								</FormControl>
+								<FormMessage />
 							</FormItem>
 						)}
 					/>
+					<div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+						<FormField
+							name='name'
+							control={form.control}
+							render={({ field }) => (
+								<FormItem className='col-span-2 md:col-span-1'>
+									<FormLabel>Name</FormLabel>
+									<FormControl>
+										<input
+											{...field}
+											disabled={isLoading}
+											placeholder='Cleopatra'
+											className='w-full rounded-md border-primary/25 border-2 p-2 focus:outline-none focus:border-primary/50'
+										/>
+									</FormControl>
+									<FormDescription>
+										Naming convention of the ai Companion
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							name='name'
+							control={form.control}
+							render={({ field }) => (
+								<FormItem className='col-span-2 md:col-span-1'>
+									<FormLabel>Description</FormLabel>
+									<FormControl>
+										<input
+											{...field}
+											disabled={isLoading}
+											placeholder='Queen of the Nile'
+											className='w-full rounded-md border-primary/25 border-2 p-2 focus:outline-none focus:border-primary/50'
+										/>
+									</FormControl>
+									<FormDescription>Description of the ai Companion</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							name='categoryId'
+							control={form.control}
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Category</FormLabel>
+									<Select
+										disabled={isLoading}
+										onValueChange={field.onChange}
+										value={field.value}
+										defaultValue={field.value}>
+										<FormControl>
+											<SelectTrigger className='bg-background'>
+												<SelectValue
+													defaultValue={field.value}
+													placeholder='Select a Category'
+												/>
+											</SelectTrigger>
+										</FormControl>
+										<SelectContent>
+											{categories.map((category) => (
+												<SelectItem key={category.id} value={category.id}>
+													{category.name}
+												</SelectItem>
+											))}
+										</SelectContent>
+									</Select>
+									<FormDescription>Select you ai Category</FormDescription>
+								</FormItem>
+							)}
+						/>
+					</div>
 				</form>
 			</Form>
 		</div>
--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -27,13 +27,16 @@ export const ImageUpload = ({
 
 	return (
 		<div className='space-y-4 w-full flex flex-col justify-center items-center'>
-			<CldUploadButton options={{ maxFiles: 1 }} uploadPreset='xguru6n0'>
+			<CldUploadButton
+				onUpload={(result: any) => onChange(result.info.secure_url)}
+				options={{ maxFiles: 1 }}
+				uploadPreset='xguru6n0'>
 				<div className='p-4 border-4 border-dashed border-primary/10 rounded-lg hover:opacity-75 tansition flex flex-col space-y-2 items-center justify-center'>
 					<div className='relative h-40 w-40'>
 						<Image
 							alt='upload'
 							fill
-							src='/next.svg'
+							src={value ||'/next.svg'}
 							className='rounded-lg object-cover'
 						/>
 					</div>
