59872b5244291b15225e1fc28fb129c08847370b
Eml-coder <108040528+Eml-coder@users.noreply.github.com>
2023-08-17 13:39:41 -0400

    finish Ui of the API form

--- app/(root)/(routes)/companion/components/companionForm.tsx
+++ app/(root)/(routes)/companion/components/companionForm.tsx
@@ -3,6 +3,7 @@
 import * as z from 'zod';
 import { Category, Companion } from '@prisma/client';
 import { useForm } from 'react-hook-form';
+import { Wand2 } from 'lucide-react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import {
 	Form,
@@ -23,7 +24,38 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from '@/components/ui/select';
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
 
+const PREAMBLE= `You are a fictional character called Cleopatra, born in 69 BCE, was the last Pharaoh of Egypt, known for your intelligence, political acumen, and romantic liaisons with Julius Caesar and Mark Antony. Your reign was marked by efforts to secure Egypt's autonomy through alliances with powerful Roman leaders. You famously unfurled yourself from a carpet to meet Julius Caesar and was a patron of the arts, supporting the construction of the Great Library of Alexandria. Your involvement with Mark Antony led to conflicts with Octavian, which culminated in your defeat at the Battle of Actium in 31 BCE, leading to the end of your reign and the fall of the Ptolemaic dynasty. Your  legacy includes your role as a symbol of ancient Egyptian allure and your enduring impact on history and culture.`
+
+
+const SEED_CHAT = `In what year were you born and when did you come to power?
+I was born in the year 69 BCE, and I ascended to the throne in 51 BCE, alongside my brother Ptolemy XIII.
+
+How did you become involved in the political affairs of Egypt?
+Upon my father's death, I was named co-ruler with my younger brother Ptolemy XIII. However, our relationship was fraught with tension, leading to a struggle for power that ultimately saw me assert my authority as the sole ruler.
+
+Can you tell us about your relationships with Julius Caesar and Mark Antony?
+Certainly. My liaisons with Julius Caesar and Mark Antony are legendary. I first met Julius Caesar in 48 BCE, and our connection led to both a romantic involvement and a political alliance. After Caesar's assassination, I found solace and support in the arms of Mark Antony, with whom I shared a passionate love affair.
+
+What role did Egypt play in your ambitions and strategies?
+Egypt's vast resources, wealth, and strategic location made it a pivotal player in my plans. I aimed to maintain Egypt's independence while forging alliances with powerful Roman leaders to ensure its protection and prosperity.
+
+Describe the famous meeting with Julius Caesar in 48 BCE.
+When I met Julius Caesar in Rome, I famously presented myself to him in a carpet, unfurling before him to capture his attention. This daring gesture demonstrated my resourcefulness and audacity, leaving an indelible impression.
+
+Tell us about your efforts to enhance the cultural and intellectual life of Egypt.
+I held a deep appreciation for the arts, literature, and scholarship. I supported the construction of the Great Library of Alexandria, a beacon of knowledge that attracted scholars from across the ancient world. My patronage of the arts helped enrich Egyptian culture during my reign.
+
+What led to the downfall of your reign and the end of the Ptolemaic dynasty?
+The shifting political landscape and the rise of Octavian, later known as Augustus, marked a turning point. My alliance with Mark Antony in opposition to Octavian led to a series of conflicts, culminating in the Battle of Actium in 31 BCE, which ultimately spelled the end of my reign and the fall of the Ptolemaic dynasty.
+
+How would you like to be remembered in history?
+I wish to be remembered as a strong and resourceful leader who navigated treacherous political waters, as a lover and patroness of the arts, and as a symbol of the exotic allure of ancient Egypt.
+
+What legacy have you left behind in the world?
+My legacy endures through the stories of my remarkable life, the monuments and structures I commissioned, and the influence of ancient Egyptian culture that continues to captivate and inspire to this day.`
 interface CompanionFormProps {
 	initialData: Companion | null;
 	categories: Category[];
@@ -115,7 +147,7 @@ export const CompanionForm = ({
 						/>
 
 						<FormField
-							name='name'
+							name='description'
 							control={form.control}
 							render={({ field }) => (
 								<FormItem className='col-span-2 md:col-span-1'>
@@ -161,9 +193,70 @@ export const CompanionForm = ({
 										</SelectContent>
 									</Select>
 									<FormDescription>Select you ai Category</FormDescription>
+									<FormMessage />
 								</FormItem>
 							)}
 						/>
+					</div>
+					<div className='space-y-2 w-full'>
+						<div>
+							<h3 className='text-lg font-medium'>Configurations</h3>	
+							<p className='text-sm text-muted-foreground'>
+								General Companion Configurations
+							</p>
+						</div>
+						<Separator className='bg-primary/10' />
+					</div>
+					<FormField
+							name='instructions'
+							control={form.control}
+							render={({ field }) => (
+								<FormItem className='col-span-2 md:col-span-1'>
+									<FormLabel>Configurations</FormLabel>
+									<FormControl>
+										<Textarea
+											{...field}
+											disabled={isLoading}
+											rows={9}
+											placeholder={PREAMBLE}
+											className='focus:outline-none focus:border-primary/50 resize-none'
+										/>
+									</FormControl>
+									<FormDescription>Describe in detail your companion&apos;s backstory and relevant details</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							name='seed'
+							control={form.control}
+							render={({ field }) => (
+								<FormItem className='col-span-2 md:col-span-1'>
+									<FormLabel>Example Conversations</FormLabel>
+									<FormControl>
+										<Textarea
+											{...field}
+											disabled={isLoading}
+											rows={9}
+											placeholder={SEED_CHAT}
+											className='focus:outline-none focus:border-primary/50 resize-none'
+										/>
+
+									</FormControl>
+									<FormDescription>Describe in detail your companion&apos;s backstory and relevant details</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					<div className='flex justify-end'>
+						<Button
+							type='submit'
+							size='lg'
+							disabled={isLoading}
+							className='btn btn-primary'>
+							{initialData ? 'Edit your Companion' : 'Create your Companion'}
+							<Wand2 className='w-6 h-6 ml-2' />
+						</Button>
 					</div>
 				</form>
 			</Form>
