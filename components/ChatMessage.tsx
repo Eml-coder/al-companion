03abb642b78aadaa57b250419f47060405981af2
Eml-coder <108040528+Eml-coder@users.noreply.github.com>
2023-09-01 14:51:10 -0400

    feat: Add loading animation for chatmessages

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -1,7 +1,9 @@
 'use client';
-
-import { useToast } from "@/components/ui/use-toast";
-import { useTheme } from "next-themes";
+import { BeatLoader } from 'react-spinners';
+import { useToast } from '@/components/ui/use-toast';
+import { cn } from '@/lib/utils';
+import { useTheme } from 'next-themes';
+import { BotAvatar } from '@/components/BotAvatar';
 
 export interface ChatMessageProps {
 	role: 'system' | 'user';
@@ -16,18 +18,32 @@ export const ChatMessage = ({
 	isLoading,
 	src,
 }: ChatMessageProps) => {
-    const { toast } = useToast();
-    const { theme } = useTheme();
+	const { toast } = useToast();
+	const { theme } = useTheme();
 
-    const onCopy = () => {
-        if (!content) {return;
-        }
-        navigator.clipboard.writeText(content);
-        toast({
-            title: 'Copied to clipboard',
-            description: 'You can paste it anywhere now',
-        
-        });
-    }
-	return <div>ChatMessage</div>;
+	const onCopy = () => {
+		if (!content) {
+			return;
+		}
+		navigator.clipboard.writeText(content);
+		toast({
+			description: 'Copied to clipboard',
+		});
+	};
+	return (
+		<div
+			className={cn(
+				'flex items-start gap-x-3 py-4 w-full',
+				role === 'user' && 'justify-end'
+			)}>
+			{role !== 'user' && src && <BotAvatar src={src} />}
+			<div className=' rounded-md px-4 py-2 max-w-sm bg-primary/10 text-primary/80'>
+				{isLoading ? (
+					<BeatLoader color={theme === 'light' ? 'black' : 'white'} size={5} />
+				) : (
+					content
+				)}
+			</div>
+		</div>
+	);
 };
--- components/ChatMessages.tsx
+++ components/ChatMessages.tsx
@@ -14,5 +14,13 @@ export const ChatMessages = ({
 	isLoading,
 	companion,
 }: ChatMessagesProps) => {
-	return <div className='flex-1 overflow-y-auto pr-4'><ChatMessage /></div>;
+	return (
+		<div className='flex-1 overflow-y-auto pr-4'>
+			<ChatMessage
+				src={companion.src}
+				role='system'
+				content={`Hello, I am ${companion.name}, ${companion.description}`}
+			/>
+		</div>
+	);
 };
